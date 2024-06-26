1f0ebbe1d361109f2213065a408b3d1b91aa9a2a
Michael Gilbert <gilbertm@mit.edu>
2024-06-26 15:09:15 -0400

    Clean up power permutation

--- combinatorics/permutation.py
+++ combinatorics/permutation.py
@@ -1,6 +1,7 @@
-from itertools import combinations, permutations
+from itertools import permutations
+from typing import Iterable
 
-def powerset_permutation(items):
+def powerset_permutation(items: Iterable):
     """The permutation of all subset of items (empty set included)."""
     for i in range(len(items)+1):
         yield from permutations(items, i)
