f683dd0af07f242a6996ed752d0eb97c7b54c36c
Michael Gilbert <gilbertm@mit.edu>
2024-07-02 19:37:35 -0400

    Finish combine_pareto

--- combinatorics/pareto.py
+++ combinatorics/pareto.py
@@ -23,6 +23,48 @@ def pareto_filter(x: np.array, y: np.array) -> np.array:
     return sort_mask[filter_mask]
 
 
+def combine_data(
+    x1: np.array,
+    y1: np.array,
+    x2: np.array,
+    y2: np.array,
+    x_combiner: Callable,
+    y_combiner: Callable
+):
+    """Returns the combined points.
+    
+    Elements of x are combined as x = x_combiner(e1, e2), e1 in x1, e2 in x2.
+    Elements of y are combined similarly.
+    Common choices for the combiner are `operator.add` and `max`
+    """
+    x1_shape = x1.shape[0]
+    x2_shape = x2.shape[0]
+
+    all_x = np.zeros(x1_shape*x2_shape)
+    all_y = np.zeros(x1_shape*x2_shape)
+
+    def make_repeated(x, n):
+        return np.tile(x, n).reshape((-1, n)).T.flatten()
+
+    def make_repeated_and_rolled(x, n):
+        x_shape = x.shape[0]
+        repeated_x = np.zeros((x_shape*n,))
+        for i in range(n):
+            repeated_x[i*x_shape:(i+1)*x_shape] = np.roll(x, i)
+        return repeated_x
+
+    # Compute all pairings
+    repeated_x1 = make_repeated(x1, x2_shape)
+    repeated_y1 = make_repeated(y1, x2_shape)
+    repeated_x2 = make_repeated_and_rolled(x2, x1_shape)
+    repeated_y2 = make_repeated_and_rolled(y2, x1_shape)
+    
+    all_x = x_combiner(repeated_x1, repeated_x2)
+    all_y = y_combiner(repeated_y1, repeated_y2)
+
+    return all_x, all_y
+
+
 def combine_pareto(
     x1: np.array,
     y1: np.array,
@@ -31,10 +73,12 @@ def combine_pareto(
     x_combiner: Callable,
     y_combiner: Callable
 ):
-    """Returns the combined Pareto curve.
+    """Returns the combined and Pareto-filtered curve.
     
     Elements of x are combined as x = x_combiner(e1, e2), e1 in x1, e2 in x2.
     Elements of y are combined similarly.
     Common choices for the combiner are `operator.add` and `max`
     """
-    pass
+    all_x, all_y = combine_data(x1, y1, x2, y2, x_combiner, y_combiner)
+    mask = pareto_filter(all_x, all_y)
+    return all_x[mask], all_y[mask]
--- tests/test_pareto.py
+++ tests/test_pareto.py
@@ -1,7 +1,9 @@
+from operator import add
 import unittest
+
 import numpy as np
 
-from combinatorics.pareto import pareto_filter
+from combinatorics.pareto import *
 
 
 class TestPareto(unittest.TestCase):
@@ -14,3 +16,67 @@ class TestPareto(unittest.TestCase):
         x = np.array([1, 3, 2, 3])
         y = np.array([3, 1, 1, 2])
         self.assertEqual([0, 2], pareto_filter(x, y).tolist())
+
+
+class TestCombineData(unittest.TestCase):
+    def test_one_to_one_add(self):
+        x1 = np.array([1, 2])
+        y1 = np.array([1, 2])
+
+        x2 = np.array([1, 2])
+        y2 = np.array([1, 2])
+
+        combined_x, combined_y = combine_data(x1, y1, x2, y2, add, add)
+
+        ref_x_to_y = {2: {2}, 3: {3}, 4: {4}}
+
+        self.check_result(ref_x_to_y, combined_x, combined_y)
+
+    def test_one_to_many_add(self):
+        x1 = np.array([1, 2])
+        y1 = np.array([2, 1])
+
+        x2 = np.array([1, 2])
+        y2 = np.array([2, 2])
+
+        combined_x, combined_y = combine_data(x1, y1, x2, y2, add, add)
+
+        ref_x_to_y = {2: {4}, 3: {3, 4}, 4: {3}}
+
+        self.check_result(ref_x_to_y, combined_x, combined_y)
+    
+    def test_max(self):
+        x1 = np.array([1, 2])
+        y1 = np.array([2, 1])
+
+        x2 = np.array([1, 2])
+        y2 = np.array([2, 2])
+
+        combined_x, combined_y = combine_data(x1, y1, x2, y2, np.maximum, add)
+
+        ref_x_to_y = {1: {4}, 2: {3, 4}}
+
+        self.check_result(ref_x_to_y, combined_x, combined_y)
+
+    def check_result(self, ref_x_to_y, combined_x, combined_y):
+        for i, x in enumerate(combined_x):
+            self.assertTrue(combined_y[i] in ref_x_to_y[x])
+
+
+class TestCombinePareto(unittest.TestCase):
+    def test_one_to_many(self):
+        x1 = np.array([1, 2])
+        y1 = np.array([2, 1])
+
+        x2 = np.array([1, 2])
+        y2 = np.array([2, 2])
+
+        combined_x, combined_y = combine_pareto(x1, y1, x2, y2, add, add)
+
+        ref_x_to_y = {2: {4}, 3: {3}}
+
+        self.check_result(ref_x_to_y, combined_x, combined_y)
+
+    def check_result(self, ref_x_to_y, combined_x, combined_y):
+        for i, x in enumerate(combined_x):
+            self.assertTrue(combined_y[i] in ref_x_to_y[x])
