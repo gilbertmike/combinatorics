2baac561192f9899a15f102697b8b83164650e7b
Michael Gilbert <gilbertm@mit.edu>
2024-07-24 14:12:51 -0400

    Add compositions

---
+++ combinatorics/compositions.py
@@ -0,0 +1,24 @@
+from collections.abc import Sequence
+from itertools import combinations
+
+
+def compositions_of_set(items: Sequence):
+    """
+    Iterates over all sets of non-overlapping, covering, contiguous
+    subsequences of `items`.
+
+    E.g., for ABC, it will iterate over four results:
+        A, B, C
+        AB, C
+        A, BC
+        ABC
+    """
+    for n_parts in range(1, len(items)+1):
+        compositions = [None]*n_parts
+        for dividers in combinations(range(1, len(items)), n_parts-1):
+            last = 0
+            for i, divider in enumerate(dividers):
+                compositions[i] = items[last:divider]
+                last = divider
+            compositions[-1] = items[last:]
+            yield tuple(compositions)
---
+++ tests/test_compositions.py
@@ -0,0 +1,18 @@
+import unittest
+
+from combinatorics.compositions import compositions_of_set
+
+
+class TestCompositions(unittest.TestCase):
+    def test_compositions_of_set(self):
+        ITEMS = 'abcd'
+
+        all_solns = set()
+        for composition in compositions_of_set(ITEMS):
+            self.assertEqual(ITEMS, ''.join(composition))
+
+            self.assertTrue(composition not in all_solns)
+            all_solns.add(composition)
+
+        # See: https://en.wikipedia.org/wiki/Composition_(combinatorics)
+        self.assertEqual(2**(len(ITEMS)-1), len(all_solns))
